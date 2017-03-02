319dff5845e952b484a9fbb463bd4045bff71232
William Olojede <wolojede@yahoo.com>
2017-03-02 05:38:45 +0300

    created app layout with components

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,14 +2,11 @@ import React from 'react';
 
 class StorePicker extends React.Component {
 	render() {
-		// return React.createElement('p', {className: 'Testing'}, 'I love you');
 		return (
-			<form className="store-collector">
-				{/* comment */}
+			<form className="store-selector">
 				<h2>Please Enter a Store</h2>
 				<input type="text" required placeholder="Store Name"/>
 				<button type="submit">Visit Store</button>
-
 			</form>
 		)
 	}
